588a5eee78593a0615182fbba2ff783e50f18662
jacekwierzchowski <jacu1010@gmail.com>
2019-04-03 20:48:45 +0200

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by Jacek Wierzchowski on 03/04/2019.
+//  Copyright © 2019 Jacek Wierzchowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
